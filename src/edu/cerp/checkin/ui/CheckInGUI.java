aa6b25e791b658a1f91a0a085aa9848e64d9e199
PabloBronee <pablobronnnee2003@gmail.com>
2025-11-03 23:57:29 -0300

    Se incorpora la GUI funcional

--- src/edu/cerp/checkin/ui/CheckInGUI.java
+++ src/edu/cerp/checkin/ui/CheckInGUI.java
@@ -15,19 +15,17 @@ public class CheckInGUI {
         ventana.setLayout(new BorderLayout());
 
         //Panel
-        JPanel panel = new JPanel(new GridLayout(4,2));
+        JPanel panel = new JPanel(new GridLayout(3,2));
 
         //Etiquetas
         JLabel nombreL = new JLabel("Ingresa el nombre");
         JLabel documentoL = new JLabel("Ingresa el documento");
         JLabel cursoL = new JLabel("Ingresa el curso");
-        JLabel fechaL = new JLabel("Ingresa la fecha");
 
         //Imputs
         JTextField nombre = new JTextField();
         JTextField documento = new JTextField();
         JTextField curso = new JTextField();
-        JTextField fecha = new JTextField();
 
         panel.add(nombreL);
         panel.add(nombre);
@@ -35,11 +33,77 @@ public class CheckInGUI {
         panel.add(documento);
         panel.add(cursoL);
         panel.add(curso);
-        panel.add(fechaL);
-        panel.add(fecha);
 
         ventana.add(panel);
 
+        //---Comenzamos con la conexion del GUI con el sistema pre establecido---
+
+        //Panel de Botones, creado y añadido
+        JPanel botones = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        JButton registrarBtn = new JButton("Registrar");
+        JButton listarBtn = new JButton("Listar");
+        JButton buscarBtn = new JButton("Buscar");
+        JButton resumenBtn = new JButton("Resumen");
+
+        botones.add(registrarBtn);
+        botones.add(listarBtn);
+        botones.add(buscarBtn);
+        botones.add(resumenBtn);
+
+        ventana.add(botones, BorderLayout.SOUTH);
+
+        //Funcionalidad del boton registrar
+        registrarBtn.addActionListener(e -> {
+            service.registrar(nombre.getText(), documento.getText(), curso.getText());
+
+            JOptionPane.showMessageDialog(ventana, "✔ Inscripción registrada");
+
+            nombre.setText("");
+            documento.setText("");
+            curso.setText("");
+        });
+
+        // Funcionalidad del boton Listar
+        listarBtn.addActionListener(e -> {
+            mostrarTabla(service.listar());
+        });
+
+        // Buscar
+        buscarBtn.addActionListener(e -> {
+            String q = JOptionPane.showInputDialog("Texto a buscar:");
+            mostrarTabla(service.buscar(q));
+        });
+
+        // Resumen
+        resumenBtn.addActionListener(e -> {
+            JOptionPane.showMessageDialog(ventana, service.resumen());
+        });
+
         ventana.setVisible(true);
     }
+
+    //Se crea un motodo "Mostrar tabla" para lidiar con el listado, y busqueda de forma mas limpia
+    private static void mostrarTabla(java.util.List<edu.cerp.checkin.model.Inscripcion> lista) {
+        JFrame frame = new JFrame("Resultados");
+        frame.setSize(500, 300);
+
+        String[] columnas = {"Nombre", "Documento", "Curso", "Fecha/Hora"};
+        String[][] datos = new String[lista.size()][4];
+
+        for (int i = 0; i < lista.size(); i++) {
+            var ins = lista.get(i);
+            datos[i][0] = ins.getNombre();
+            datos[i][1] = ins.getDocumento();
+            datos[i][2] = ins.getCurso();
+            datos[i][3] = ins.getFechaHora().toString();
+        }
+
+        JTable tabla = new JTable(datos, columnas);
+        frame.add(new JScrollPane(tabla));
+
+        frame.setVisible(true);
+    }
+
+
 }
